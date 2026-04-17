a9b6ac8278fdcdf9c6b2e227cb70ff87e14d822b
ermyltsor <lamiimuk@163.com>
2026-04-17 20:55:39 +0800

    jin: Decommonize nfc parts
    Change-Id: Ic89830836fe21a291ea944a7b5aad7aac195abf5

--- extract-files.py
+++ extract-files.py
@@ -98,6 +98,11 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('remote_handle64_invoke')
         .clear_symbol_version('remote_handle64_open')
         .clear_symbol_version('remote_register_dma_handle'),
+    ('odm/etc/libnfc-mtp-SN100.conf_20161', 'odm/etc/libnfc-mtp-SN100.conf_20351'): blob_fixup()
+        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
+    'vendor/etc/libnfc-nxp.conf': blob_fixup()
+        .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
+        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so')
         .binary_regex_replace(b'com.oem.autotest', b'\x00om.oem.autotest'),
