51ede990ca30e7ab6a43cc2240d2476715b79d43
ermyltsor <lamiimuk@163.com>
2026-04-18 00:49:27 +0800

    jin: Import oplus camera config from stock
    Change-Id: I73969dfb6f62d6dae370160f425c0b3a5d07c823

--- device.mk
+++ device.mk
@@ -46,6 +46,9 @@ PRODUCT_COPY_FILES += \
 # Oplus camera
 $(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/camera/oplus_camera_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_config
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
